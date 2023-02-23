356a63df33b4b351d0487ff4a10b5fe268a82c4d
Janett Gerrard <mikanchik@gmail.com>
2023-02-23 15:20:10 +0000

    user can add and delete bookmarks from lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: %i(index show new create)
+  resources :lists, only: %i(index show new create) do
+    resources :bookmarks, only: %i(new create)
+  end
+
+  resources :bookmarks, only: :destroy
 end
