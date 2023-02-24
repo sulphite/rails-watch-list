59ff65cdb00ddbfc86cdb2c7bad92bce35b4ee8b
Janett Gerrard <mikanchik@gmail.com>
2023-02-24 15:26:36 +0000

    precompile assets

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-CLOUDINARY_URL=cloudinary://814775841623682:xuSuTF2Xc3-qCcnKWzuokq6ylb0@drtgubiyn
+CLOUDINARY_URL="cloudinary://814775841623682:xuSuTF2Xc3-qCcnKWzuokq6ylb0@drtgubiyn"
