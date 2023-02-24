639c869505357d6590f96581bece2e6d745f72ce
Janett Gerrard <mikanchik@gmail.com>
2023-02-24 10:23:27 +0000

    cleaning up

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="tom-select"
 export default class extends Controller {
   connect() {
+    new TomSelect(this.element)
   }
 }
